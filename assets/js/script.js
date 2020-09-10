b617a8a6b3b135e3665b127797556000a0822a11
Richy Rich <richardflores009@gmail.com>
2020-09-10 16:41:59 -0700

    local storage for genre functional

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,9 @@ var posterEl = document.querySelector(".poster");
 var historyEl = document.querySelector(".history");
 var recipeContainerEl = document.querySelector(".recipe-cont");
 var movieName = JSON.parse(localStorage.getItem("movies")) || [];
-var genreName = JSON.parse(localStorage.getItem("genre")) || [];
-var rMovieApiKey = "&apiKey=0c7c604f01a143d598df0735356390c3";
-var rRecipeApiKey = "apikey=84c248ca";
+var genreName = localStorage.getItem("genre")
+var rRecipeApiKey = "&apiKey=0c7c604f01a143d598df0735356390c3";
+var rMovieApiKey = "apikey=84c248ca";
 
 // Local Storage Function for Movie Search History
 var historyStorage = function(name) {
@@ -33,7 +33,7 @@ var genreStorage = function(genre) {
 // API Fetches
 // Movie Poster Fetch
 var getMoviePoster = function(movie) {
-    var apiUrl = `http://www.omdbapi.com/?${rRecipeApiKey}&t=jaws`;
+    var apiUrl = `http://www.omdbapi.com/?${rMovieApiKey}&t=superbad`;
     
 
     fetch(apiUrl)
@@ -48,9 +48,10 @@ var getMoviePoster = function(movie) {
         var moviePoster = data.Poster
         var moviePlot = data.Plot
 
-        //set movie genre to local storage
-        genreName = movieGenre
-        movieName = movieTitle
+        //set movie genre to searchhandler
+        searchHandler(movieGenre)
+        
+        // movieName = movieTitle
     })
     // Alert modal user that fetch was not successful
     // pass response to movie poster display function
@@ -111,21 +112,22 @@ var displayRecipes = function(recipedata) {
     // Expand Recipe Size Hover
 
 // // Global Function | Get Local Storage Names and Pass Into Functions
-// var searchHandler = function(genreValue) {
-//     var searchItem = document.querySelector(".movie-input").value.trim();
-
-//     if (searchItem) {
-//         getMoviePoster(searchItem);
-//         historyStorage(searchItem);
-//         document.querySelector('.movie-input').value = ""
-//     }
+var searchHandler = function(genreValue) {
+    // var searchItem = document.querySelector(".movie-input").value.trim();
+    console.log(genreValue)
+    // if (searchItem) {
+    //     getMoviePoster(searchItem);
+    //     historyStorage(searchItem);
+    //     document.querySelector('.movie-input').value = ""
+    // }
     
-//     if (genreValue) {
-//         getRecipes(genreValue);
-//     }
+    if (genreValue) {
+        // getRecipes(genreValue);
+        genreStorage(genreValue)
+    }
     
 
-// };
+};
 
 // //Event Listener for Submit Button
 // submitBtnEl.addEventListener("click", searchHandler)
