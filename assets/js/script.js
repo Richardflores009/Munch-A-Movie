d24ab41f25304941b48c2cdabfb9beaf881f182c
Richy Rich <richardflores009@gmail.com>
2020-09-10 16:20:59 -0700

    begin to push genre and title to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,8 @@ var submitBtnEl = document.querySelector(".btn");
 var posterEl = document.querySelector(".poster");
 var historyEl = document.querySelector(".history");
 var recipeContainerEl = document.querySelector(".recipe-cont");
-var movieName = JSON.parse(localStorage.getItem("movies"));
-var genreName = JSON.parse(localStorage.getItem("genre"));
+var movieName = JSON.parse(localStorage.getItem("movies")) || [];
+var genreName = JSON.parse(localStorage.getItem("genre")) || [];
 var rMovieApiKey = "&apiKey=0c7c604f01a143d598df0735356390c3";
 var rRecipeApiKey = "apikey=84c248ca";
 
@@ -33,65 +33,71 @@ var genreStorage = function(genre) {
 // API Fetches
 // Movie Poster Fetch
 var getMoviePoster = function(movie) {
-    var apiUrl = `http://www.omdbapi.com/?${rRecipeApiKey}&t=${movie}`;
+    var apiUrl = `http://www.omdbapi.com/?${rRecipeApiKey}&t=jaws`;
+    
 
-
-    fetch(apiUrl).then(function(response) {
+    fetch(apiUrl)
+    .then(function(response) {
         //request was successful
-        if(response.ok) {
-            response.json().then(function(moviedata) {
-                // pass response to movie poster display function
-                displayPoster(moviedata);
-            });
-        } else {
-            // Alert modal user that fetch was not successful
-        }
-    });
-};
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data)
+        var movieTitle = data.Title
+        var movieGenre = data.Genre.split(',').pop()
+        var moviePoster = data.Poster
+        var moviePlot = data.Plot
 
+        //set movie genre to local storage
+        genreName = movieGenre
+        movieName = movieTitle
+    })
+    // Alert modal user that fetch was not successful
+    // pass response to movie poster display function
+};
+getMoviePoster()
 //Recipe Fetch
 // If Statement for Pairing Movie Genre with Ingredients
 
 //Fetch
-var getRecipes = function(meal) {
-    var api = `https://api.spoonacular.com/recipes/random?number=3&tags=chinese` + rMovieApiKey;
+// var getRecipes = function(meal) {
+//     var api = `https://api.spoonacular.com/recipes/random?number=3&tags=chinese` + rMovieApiKey;
 
 
-        fetch(api)
-        .then(function(response) {
+//         fetch(api)
+//         .then(function(response) {
             
-        //request as successful
-            return response.json();
-        })
-        .then(function(data){
-            for (let i = 0; i < data.recipes.length; i++){
-                console.log(data)
-                // Recipe name
-                var recipeName = data.recipes[1].title
+//         //request as successful
+//             return response.json();
+//         })
+//         .then(function(data){
+//             for (let i = 0; i < data.recipes.length; i++){
+//                 // Recipe name
+//                 var recipeName = data.recipes[1].title
 
-                // Food image
-                var recipeImage = data.recipes[1].image
+//                 // Food image
+//                 var recipeImage = data.recipes[1].image
 
-                // recipe link
-                var recipeLink = data.recipes[0].sourceUrl
+//                 // recipe link
+//                 var recipeLink = data.recipes[0].sourceUrl
 
-                // ingredient loop
-                for (let ing = 0; ing < data.recipes[0].extendedIngredients.length; ing++) {
-                    var ingList = data.recipes[0].extendedIngredients[ing].name
-                    var ingamount = data.recipes[0].extendedIngredients[ing].amount
-                    var ingUnits = data.recipes[0].extendedIngredients[ing].measures.us.unitLong
-                    var ingShop = ingamount + ' ' + ingUnits
-                }
+//                 // ingredient loop
+//                 for (let ing = 0; ing < data.recipes[0].extendedIngredients.length; ing++) {
+//                     var ingList = data.recipes[0].extendedIngredients[ing].name
+//                     var ingamount = data.recipes[0].extendedIngredients[ing].amount
+//                     var ingUnits = data.recipes[0].extendedIngredients[ing].measures.us.unitLong
+//                     var ingShop = ingamount + ' ' + ingUnits
+//                 }
 
-                // instruction loop
-                for (let inst = 0; inst < data.recipes[0].analyzedInstructions[0].steps.length; inst++){
-                    var cookSteps = data.recipes[0].analyzedInstructions[0].steps[inst].step
-                }
-            }
-        })
+//                 // instruction loop
+//                 for (let inst = 0; inst < data.recipes[0].analyzedInstructions[0].steps.length; inst++){
+//                     var cookSteps = data.recipes[0].analyzedInstructions[0].steps[inst].step
+//                 }
+//             }
+//         })
         
-};
-getRecipes()
+// };
+// getRecipes()
 // Display Movie Poster
 var displayPoster = function(moviedata) {
 
